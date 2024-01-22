f916493cfa47d9fd2201ecfd00883e28d3d5512d
Chubi Best <you@example.com>
2024-01-22 17:02:25 +0100

    fix build?

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedinIn, faMediumM, faStackOverflow, faCodepen, faNeos } from '@fortawesome/free-brands-svg-icons';
-import { faCode, faTree } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedinIn, faMediumM, faStackOverflow, faNeos } from '@fortawesome/free-brands-svg-icons';
+import { faTree } from '@fortawesome/free-solid-svg-icons';
 
 import styled from 'styled-components';
 
